11e883f4dd698478fdecdde26d08f70a5f5db5f6
Rangel <raspasov@gmail.com>
2015-04-10 11:04:08 -0700

    bump amazon java library version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject amazonica "0.3.19"
+(defproject com.raspasov/amazonica "0.3.19"
   :description "A comprehensive Clojure client for the entire Amazon AWS api."
-  :url "https://github.com/mcohen01/amazonica"
+  :url "https://github.com/raspasov/amazonica"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["src/main/java"]
@@ -9,7 +9,7 @@
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/algo.generic "0.1.2"]
-                 [com.amazonaws/aws-java-sdk "1.9.13" :exclusions [joda-time]]
+                 [com.amazonaws/aws-java-sdk "1.9.30" :exclusions [joda-time]]
                  [org.apache.httpcomponents/httpclient "4.3.3"]
                  [com.amazonaws/amazon-kinesis-client "1.1.0" :exclusions [joda-time]]
                  [joda-time "2.2"]
